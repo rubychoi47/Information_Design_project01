65f73ce95a96bf439f1f4f082111e9d4f7837065
hyeon <nguswjd02@gmail.com>
2025-11-02 00:15:35 +0900

    feat: layout 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
 import "./index.css";
 
 import Header from "./components/header";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "./components/card";
 
 function App() {
   return (
-    <>
+    <div className="w-full h-screen">
       <Header />
-    </>
+      <main className="w-full h-full flex flex-col items-center  justify-center">
+        <section className="flex gap-12">
+          <h2 className="hidden">card</h2>
+          <Card>
+            <CardHeader>
+              <CardTitle>Net revenue</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>$3,342,868,034</p>
+            </CardContent>
+            <CardFooter trend="up" value="10.92%" footerText="Last month" />
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Net revenue</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>$3,342,868,034</p>
+            </CardContent>
+            <CardFooter trend="up" value="10.92%" footerText="Last month" />
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Net revenue</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>$3,342,868,034</p>
+            </CardContent>
+            <CardFooter trend="up" value="10.92%" footerText="Last month" />
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Net revenue</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>$3,342,868,034</p>
+            </CardContent>
+            <CardFooter trend="up" value="10.92%" footerText="Last month" />
+          </Card>
+        </section>
+      </main>
+    </div>
   );
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { SignalHigh } from "lucide-react";
 
 function Header() {
+  const [active, setActive] = useState<"Injective" | "Osmosis">("Injective");
+
   return (
-    <header className="w-full h-25 border-2 border-gray-5 flex items-center justify-between px-18">
+    <header className="w-full h-25 border-b-2 border-gray-5 flex items-center justify-between px-18">
       <h1 className="font-semibold text-4xl">Dashboard</h1>
       <div className="flex gap-4.5">
-        <Button children="Injective" icon={SignalHigh} />
-        <Button children="Osmosis" icon={SignalHigh} />
+        <Button
+          variant={active === "Injective" ? "active" : "default"}
+          onClick={() => setActive("Injective")}
+          icon={SignalHigh}
+        >
+          Injective
+        </Button>
+        <Button
+          variant={active === "Osmosis" ? "active" : "default"}
+          onClick={() => setActive("Osmosis")}
+          icon={SignalHigh}
+        >
+          Osmosis
+        </Button>
       </div>
     </header>
   );
