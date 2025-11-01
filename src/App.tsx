61108a7f5049fbcf291b8e4986f228c71667dd8b
hyeon <nguswjd02@gmail.com>
2025-11-02 00:22:35 +0900

    깃 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import DynamicChart from "./components/ui/graph";
 function App() {
   return (
     <>
-      <h1 className="text-blue-1 ">information design project </h1>
+      <h1 className="text-blue-1 ">information design project d</h1>
       <Button children="Injective" icon={SignalHigh} />
 
       <GraphCard>
